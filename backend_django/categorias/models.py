a7c226466053466e9e4885eee0065056c0ceb9ae
HeillyMM <111702460+HeillyMM@users.noreply.github.com>
2026-06-26 17:30:42 -0400

    Delete backend_django directory

--- backend_django/categorias/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Categoria(models.Model):
-    nombre = models.CharField(max_length=100)
-    class Meta:
-        db_table = 'tbl_categoria'
-
-    def __str__(self):
-        return self.nombre
--- backend_django/categorias/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.categoria_list),
-    path('<int:pk>/', views.categoria_detail),
-]
